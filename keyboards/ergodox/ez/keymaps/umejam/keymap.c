444264d9ded7a587565f754e447b9de11a253e30
Takeshi Imai <imai-t@casio.co.jp>
2017-06-07 09:39:04 +0900

    CtrlとEnterの位置を変更

--- keyboards/ergodox/ez/keymaps/umejam/keymap.c
+++ keyboards/ergodox/ez/keymaps/umejam/keymap.c
@@ -18,11 +18,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /* Keymap 0: Basic layer
  *
  * ,--------------------------------------------------.           ,--------------------------------------------------.
- * |   =    |   1  |   2  |   3  |   4  |   5  | ~    |           | `    |   6  |   7  |   8  |   9  |   0  |   -    |
+ * |   ESC  |   1  |   2  |   3  |   4  |   5  | `    |           |  L2  |   6  |   7  |   8  |   9  |   0  |   -    |
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
- * | Tab    |   Q  |   W  |   E  |   R  |   T  | Alt  |           |  L1  |   Y  |   U  |   I  |   O  |   P  |   \    |
+ * | Tab    |   Q  |   W  |   E  |   R  |   T  | Alt  |           |  L1  |   Y  |   U  |   I  |   O  |   P  |   =    |
  * |--------+------+------+------+------+------| Spc  |           |      |------+------+------+------+------+--------|
- * |  '     |   A  |   S  |   D  |   F  |   G  |------|           |------|   H  |   J  |   K  |   L  |; / L2| Enter  |
+ * |'/LCtrl |   A  |   S  |   D  |   F  |   G  |------|           |------|   H  |   J  |   K  |   L  |  ;   |\/RCtrl |
  * |--------+------+------+------+------+------|  [   |           |  ]   |------+------+------+------+------+--------|
  * | LShift |   Z  |   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |  /   | RShift |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
@@ -32,7 +32,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                                        | LAlt | LGui |       | RGui | RAlt   |
  *                                 ,------|------|------|       |------+--------+------.
  *                                 |      |      | RESET|       | BkSpc|        |      |
- *                                 |Space/| Esc/ |------|       |------|  Esc/  |Space/|
+ *                                 |Enter/| Esc/ |------|       |------|  Esc/  |Space/|
  *                                 |LCtrl | LGui |CtlGui|       |CtlGui|  RGui  |RCtrl |
  *                                 `--------------------'       `----------------------'
  */
@@ -40,18 +40,18 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 // Otherwise, it needs KC_*
 [BASE] = KEYMAP(  // layer 0 : default
         // left hand
-        KC_EQL,         KC_1,         KC_2,   KC_3,   KC_4,   KC_5,   KC_TILD,
+        KC_ESC,         KC_1,         KC_2,   KC_3,   KC_4,   KC_5,   KC_GRV,
         KC_TAB,         KC_Q,         KC_W,   KC_E,   KC_R,   KC_T,   LALT(KC_SPC),
-        KC_QUOT,        KC_A,         KC_S,   KC_D,   KC_F,   KC_G,
+        LCTL_T(KC_QUOT),KC_A,         KC_S,   KC_D,   KC_F,   KC_G,
         KC_LSFT,        KC_Z,         KC_X,   KC_C,   KC_V,   KC_B,   KC_LBRC,
         KC_HOME,        ALL_T(KC_NO), MEH_T(KC_NO),LALT(KC_SPC),KC_LANG1,
                                                       KC_LALT,KC_LGUI,
                                                               RESET,
-                          LCTL_T(KC_SPC),LGUI_T(KC_ESC),LGUI(KC_LCTL),
+                          LCTL_T(KC_ENT),LGUI_T(KC_ESC),LGUI(KC_LCTL),
         // right hand
-             KC_GRV,      KC_6,   KC_7,  KC_8,   KC_9,   KC_0,             KC_MINS,
-             TG(SYMB),    KC_Y,   KC_U,  KC_I,   KC_O,   KC_P,             KC_BSLS,
-                          KC_H,   KC_J,  KC_K,   KC_L,   LT(MDIA, KC_SCLN),KC_ENT,
+             TG(MDIA),    KC_6,   KC_7,  KC_8,   KC_9,   KC_0,             KC_MINS,
+             TG(SYMB),    KC_Y,   KC_U,  KC_I,   KC_O,   KC_P,             KC_EQL,
+                          KC_H,   KC_J,  KC_K,   KC_L,   KC_SCLN,          RCTL_T(KC_BSLS),
              KC_RBRC,     KC_N,   KC_M,  KC_COMM,KC_DOT, KC_SLSH,          KC_RSFT,
                                            KC_LANG2, KC_LEFT,KC_DOWN,KC_UP,KC_RIGHT,
         KC_RGUI, KC_RALT,
@@ -84,7 +84,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
        // left hand
        VRSN,   KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_F5,  KC_TRNS,
        KC_TRNS,KC_EXLM,KC_AT,  KC_LCBR,KC_RCBR,KC_PIPE,KC_TRNS,
-       KC_TRNS,KC_HASH,KC_DLR, KC_LPRN,KC_RPRN,KC_GRV,
+       KC_LEFT,KC_HASH,KC_DLR, KC_LPRN,KC_RPRN,KC_GRV,
        KC_TRNS,KC_PERC,KC_CIRC,KC_LBRC,KC_RBRC,KC_TILD,KC_TRNS,
           EPRM,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,
                                        RGB_MOD,KC_TRNS,
@@ -93,7 +93,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
        // right hand
        KC_TRNS, KC_F6,   KC_F7,  KC_F8,   KC_F9,   KC_F10,  KC_F11,
        KC_TRNS, KC_UP,   KC_7,   KC_8,    KC_9,    KC_ASTR, KC_F12,
-                KC_DOWN, KC_4,   KC_5,    KC_6,    KC_PLUS, KC_TRNS,
+                KC_DOWN, KC_4,   KC_5,    KC_6,    KC_PLUS, KC_RIGHT,
        KC_TRNS, KC_AMPR, KC_1,   KC_2,    KC_3,    KC_BSLS, KC_TRNS,
                          KC_LEFT,KC_DOT,  KC_0,    KC_EQL,  KC_RIGHT,
        RGB_TOG, RGB_SLD,

09c0fb8444c5522a3b931ddf250afdb5e71928cf
Takeshi Imai <imai-t@casio.co.jp>
2017-06-20 13:54:52 +0900

    小指位置のコントロールキーを廃止

--- keyboards/ergodox/ez/keymaps/umejam/keymap.c
+++ keyboards/ergodox/ez/keymaps/umejam/keymap.c
@@ -22,7 +22,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
  * | Tab    |   Q  |   W  |   E  |   R  |   T  | Alt  |           |  L1  |   Y  |   U  |   I  |   O  |   P  |   =    |
  * |--------+------+------+------+------+------| Spc  |           |      |------+------+------+------+------+--------|
- * |'/LCtrl |   A  |   S  |   D  |   F  |   G  |------|           |------|   H  |   J  |   K  |   L  |  ;   |\/RCtrl |
+ * |   '    |   A  |   S  |   D  |   F  |   G  |------|           |------|   H  |   J  |   K  |   L  |  ;   |   \    |
  * |--------+------+------+------+------+------|  [   |           |  ]   |------+------+------+------+------+--------|
  * | LShift |   Z  |   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |  /   | RShift |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
@@ -42,7 +42,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         // left hand
         KC_ESC,         KC_1,         KC_2,   KC_3,   KC_4,   KC_5,   KC_GRV,
         KC_TAB,         KC_Q,         KC_W,   KC_E,   KC_R,   KC_T,   LALT(KC_SPC),
-        LCTL_T(KC_QUOT),KC_A,         KC_S,   KC_D,   KC_F,   KC_G,
+        KC_QUOT,        KC_A,         KC_S,   KC_D,   KC_F,   KC_G,
         KC_LSFT,        KC_Z,         KC_X,   KC_C,   KC_V,   KC_B,   KC_LBRC,
         KC_HOME,        ALL_T(KC_NO), MEH_T(KC_NO),LALT(KC_SPC),KC_LANG1,
                                                       KC_LALT,KC_LGUI,
@@ -51,7 +51,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         // right hand
              TG(MDIA),    KC_6,   KC_7,  KC_8,   KC_9,   KC_0,             KC_MINS,
              TG(SYMB),    KC_Y,   KC_U,  KC_I,   KC_O,   KC_P,             KC_EQL,
-                          KC_H,   KC_J,  KC_K,   KC_L,   KC_SCLN,          RCTL_T(KC_BSLS),
+                          KC_H,   KC_J,  KC_K,   KC_L,   KC_SCLN,          KC_BSLS,
              KC_RBRC,     KC_N,   KC_M,  KC_COMM,KC_DOT, KC_SLSH,          KC_RSFT,
                                            KC_LANG2, KC_LEFT,KC_DOWN,KC_UP,KC_RIGHT,
         KC_RGUI, KC_RALT,

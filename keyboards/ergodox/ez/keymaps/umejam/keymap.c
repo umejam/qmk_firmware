c635ba6ae4f16f6d9f4a238eda6c1bceddec5185
Takeshi Imai <imai-t@casio.co.jp>
2017-06-30 09:26:59 +0900

    Karabiner-Elementsの拡張機能に合わせるため、LANG1/2の位置を交換

--- keyboards/ergodox/ez/keymaps/umejam/keymap.c
+++ keyboards/ergodox/ez/keymaps/umejam/keymap.c
@@ -26,7 +26,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+------+------+------+------+------|  [   |           |  ]   |------+------+------+------+------+--------|
  * | LShift |   Z  |   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |  /   | RShift |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
- *   |Home  |Hyper | Meh  |AltSpc|LANG1 |                                       |LANG2 | Left | Down |  Up  | Right |
+ *   |Home  |Hyper | Meh  |AltSpc|LANG2 |                                       |LANG1 | Left | Down |  Up  | Right |
  *   `----------------------------------'                                       `----------------------------------'
  *                                        ,-------------.       ,---------------.
  *                                        | LAlt | LGui |       | RGui | RAlt   |
@@ -44,7 +44,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,         KC_Q,         KC_W,   KC_E,   KC_R,   KC_T,   LALT(KC_SPC),
         KC_QUOT,        KC_A,         KC_S,   KC_D,   KC_F,   KC_G,
         KC_LSFT,        KC_Z,         KC_X,   KC_C,   KC_V,   KC_B,   KC_LBRC,
-        KC_HOME,        ALL_T(KC_NO), MEH_T(KC_NO),LALT(KC_SPC),KC_LANG1,
+        KC_HOME,        ALL_T(KC_NO), MEH_T(KC_NO),LALT(KC_SPC),KC_LANG2,
                                                       KC_LALT,KC_LGUI,
                                                               RESET,
                           LCTL_T(KC_SPC),LGUI_T(KC_ENT),LGUI(KC_LCTL),
@@ -53,7 +53,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
              TG(SYMB),    KC_Y,   KC_U,  KC_I,   KC_O,   KC_P,             KC_EQL,
                           KC_H,   KC_J,  KC_K,   KC_L,   KC_SCLN,          KC_BSLS,
              KC_RBRC,     KC_N,   KC_M,  KC_COMM,KC_DOT, KC_SLSH,          KC_RSFT,
-                                           KC_LANG2, KC_LEFT,KC_DOWN,KC_UP,KC_RIGHT,
+                                           KC_LANG1, KC_LEFT,KC_DOWN,KC_UP,KC_RIGHT,
         KC_RGUI, KC_RALT,
         KC_BSPC,
         RGUI(KC_RCTL),RGUI_T(KC_ESC), RCTL_T(KC_SPC)

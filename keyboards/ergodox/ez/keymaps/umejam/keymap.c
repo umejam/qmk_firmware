234595d353367a05c0f9ef7c81815e2aa7748947
Takeshi Imai <imai-t@casio.co.jp>
2017-07-12 09:18:09 +0900

    Enter及び"'","\"の位置変更

--- keyboards/ergodox/ez/keymaps/umejam/keymap.c
+++ keyboards/ergodox/ez/keymaps/umejam/keymap.c
@@ -22,18 +22,18 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
  * | Tab    |   Q  |   W  |   E  |   R  |   T  | Alt  |           |  L1  |   Y  |   U  |   I  |   O  |   P  |   =    |
  * |--------+------+------+------+------+------| Spc  |           |      |------+------+------+------+------+--------|
- * |   '    |   A  |   S  |   D  |   F  |   G  |------|           |------|   H  |   J  |   K  |   L  |  ;   |   \    |
+ * |   \    |   A  |   S  |   D  |   F  |   G  |------|           |------|   H  |   J  |   K  |   L  |  ;   |   '    |
  * |--------+------+------+------+------+------|  [   |           |  ]   |------+------+------+------+------+--------|
  * | LShift |   Z  |   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |  /   | RShift |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
- *   |Home  |Hyper | Meh  |AltSpc|LANG2 |                                       |LANG1 | Left | Down |  Up  | Right |
+ *   |Home  |Hyper | Meh  |CtlGui|LANG2 |                                       |LANG1 | Left | Down |  Up  | Right |
  *   `----------------------------------'                                       `----------------------------------'
  *                                        ,-------------.       ,---------------.
  *                                        | LAlt | LGui |       | RGui | RAlt   |
  *                                 ,------|------|------|       |------+--------+------.
  *                                 |      |      | RESET|       | BkSpc|        |      |
- *                                 |Space/|Enter/|------|       |------|  Esc/  |Space/|
- *                                 |LCtrl | LGui |CtlGui|       |CtlGui|  RGui  |RCtrl |
+ *                                 |Space/| Esc/ |------|       |------|  Esc/  |Space/|
+ *                                 |LCtrl | LGui | Enter|       | Enter|  RGui  |RCtrl |
  *                                 `--------------------'       `----------------------'
  */
 // If it accepts an argument (i.e, is a function), it doesn't need KC_.
@@ -42,21 +42,21 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         // left hand
         KC_ESC,         KC_1,         KC_2,   KC_3,   KC_4,   KC_5,   KC_GRV,
         KC_TAB,         KC_Q,         KC_W,   KC_E,   KC_R,   KC_T,   LALT(KC_SPC),
-        KC_QUOT,        KC_A,         KC_S,   KC_D,   KC_F,   KC_G,
+        KC_BSLS,        KC_A,         KC_S,   KC_D,   KC_F,   KC_G,
         KC_LSFT,        KC_Z,         KC_X,   KC_C,   KC_V,   KC_B,   KC_LBRC,
-        KC_HOME,        ALL_T(KC_NO), MEH_T(KC_NO),LALT(KC_SPC),KC_LANG2,
+        KC_HOME,        ALL_T(KC_NO), MEH_T(KC_NO),LGUI(KC_LCTL),KC_LANG2,
                                                       KC_LALT,KC_LGUI,
                                                               RESET,
-                          LCTL_T(KC_SPC),LGUI_T(KC_ENT),LGUI(KC_LCTL),
+                         		LCTL_T(KC_SPC),LGUI_T(KC_ESC),KC_ENT,
         // right hand
              TG(MDIA),    KC_6,   KC_7,  KC_8,   KC_9,   KC_0,             KC_MINS,
              TG(SYMB),    KC_Y,   KC_U,  KC_I,   KC_O,   KC_P,             KC_EQL,
-                          KC_H,   KC_J,  KC_K,   KC_L,   KC_SCLN,          KC_BSLS,
+                          KC_H,   KC_J,  KC_K,   KC_L,   KC_SCLN,          KC_QUOT,
              KC_RBRC,     KC_N,   KC_M,  KC_COMM,KC_DOT, KC_SLSH,          KC_RSFT,
                                            KC_LANG1, KC_LEFT,KC_DOWN,KC_UP,KC_RIGHT,
         KC_RGUI, KC_RALT,
         KC_BSPC,
-        RGUI(KC_RCTL),RGUI_T(KC_ESC), RCTL_T(KC_SPC)
+        KC_ENT,RGUI_T(KC_ESC), RCTL_T(KC_SPC)
     ),
 /* Keymap 1: Symbol Layer
  *

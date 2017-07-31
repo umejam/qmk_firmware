f0d7963e523e676c5a3a67019d5b3cd0e7a31cad
Takeshi Imai <imai-t@casio.co.jp>
2017-07-31 10:49:37 +0900

    Enter位置変更

--- keyboards/ergodox/ez/keymaps/umejam/keymap.c
+++ keyboards/ergodox/ez/keymaps/umejam/keymap.c
@@ -20,10 +20,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,--------------------------------------------------.           ,--------------------------------------------------.
  * |   ESC  |   1  |   2  |   3  |   4  |   5  | `    |           |  -   |   6  |   7  |   8  |   9  |   0  | BkSpc  |
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
- * | Tab    |   Q  |   W  |   E  |   R  |   T  |  [   |           |  ]   |   Y  |   U  |   I  |   O  |   P  |   =    |
- * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
+ * | Tab    |   Q  |   W  |   E  |   R  |   T  |  [   |           | CAPS |   Y  |   U  |   I  |   O  |   P  |   =    |
+ * |--------+------+------+------+------+------|      |           | Lock |------+------+------+------+------+--------|
  * |   \    |   A  |   S  |   D  |   F  |   G  |------|           |------|   H  |   J  |   K  |   L  |  ;   |   '    |
- * |--------+------+------+------+------+------| Enter|           |Enter |------+------+------+------+------+--------|
+ * |--------+------+------+------+------+------|  ]   |           | ESC  |------+------+------+------+------+--------|
  * | LShift |   Z  |   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |  /   | RShift |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
  *   |Home  |Hyper | Meh  |CtlGui|LANG2 |                                       |LANG1 | Left | Down |  Up  | Right |
@@ -32,7 +32,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                                        | LAlt | LGui |       | RGui | RAlt   |
  *                                 ,------|------|------|       |------+--------+------.
  *                                 |      |      | RESET|       | L2   |        |      |
- *                                 |Space/| Esc/ |------|       |------|  Esc/  |Space/|
+ *                                 |Space/|Enter/|------|       |------| Enter/ |Space/|
  *                                 |LCtrl | LGui |AltSpc|       | L1   |  RGui  |RCtrl |
  *                                 `--------------------'       `----------------------'
  */
@@ -43,20 +43,20 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_ESC,         KC_1,         KC_2,   KC_3,   KC_4,   KC_5,   KC_GRV,
         KC_TAB,         KC_Q,         KC_W,   KC_E,   KC_R,   KC_T,   KC_LBRC,
         KC_BSLS,        KC_A,         KC_S,   KC_D,   KC_F,   KC_G,
-        KC_LSFT,        KC_Z,         KC_X,   KC_C,   KC_V,   KC_B,   KC_ENT,
+        KC_LSFT,        KC_Z,         KC_X,   KC_C,   KC_V,   KC_B,   KC_RBRC,
         KC_HOME,        ALL_T(KC_NO), MEH_T(KC_NO),LGUI(KC_LCTL),KC_LANG2,
                                                              KC_LALT,KC_LGUI,
                                                                        RESET,
-                    		      LCTL_T(KC_SPC),LGUI_T(KC_ESC),LALT(KC_SPC),
+                    		      LCTL_T(KC_SPC),LGUI_T(KC_ENT),LALT(KC_SPC),
         // right hand
         KC_MINS,     KC_6,   KC_7,  KC_8,   KC_9,   KC_0,             KC_BSPC,
-        KC_RBRC,     KC_Y,   KC_U,  KC_I,   KC_O,   KC_P,             KC_EQL,
+        KC_CAPS,     KC_Y,   KC_U,  KC_I,   KC_O,   KC_P,             KC_EQL,
                      KC_H,   KC_J,  KC_K,   KC_L,   KC_SCLN,          KC_QUOT,
-        KC_ENT,      KC_N,   KC_M,  KC_COMM,KC_DOT, KC_SLSH,          KC_RSFT,
+        KC_ESC,      KC_N,   KC_M,  KC_COMM,KC_DOT, KC_SLSH,          KC_RSFT,
                                      KC_LANG1, KC_LEFT,KC_DOWN,KC_UP,KC_RIGHT,
         KC_RGUI, KC_RALT,
         TG(MDIA),
-        TG(SYMB),RGUI_T(KC_ESC), RCTL_T(KC_SPC)
+        TG(SYMB),RGUI_T(KC_ENT), RCTL_T(KC_SPC)
     ),
 /* Keymap 1: Symbol Layer
  *

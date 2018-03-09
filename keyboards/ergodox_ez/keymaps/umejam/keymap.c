33d0608db1fadb45542a846d9dac5983ff5fa245
Takeshi Imai <imai-t@casio.co.jp>
2018-03-09 11:55:43 +0900

    CAPS LOCK移動

--- keyboards/ergodox_ez/keymaps/umejam/keymap.c
+++ keyboards/ergodox_ez/keymaps/umejam/keymap.c
@@ -29,11 +29,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *   |Home  |Hyper | Meh  |CtlGui|LANG2 |                                       |LANG1 | Left | Down |  Up  | Right |
  *   `----------------------------------'                                       `----------------------------------'
  *                                        ,-------------.       ,---------------.
- *                                        | LAlt | LGui |       | RGui | RAlt   |
+ *                                        | CAPS | LGui |       | RGui |  L1    |
  *                                 ,------|------|------|       |------+--------+------.
  *                                 |      |      | RESET|       | L2   |        |      |
  *                                 |Space/|Enter/|------|       |------| Enter/ |Space/|
- *                                 |LCtrl | LGui | CAPS |       | L1   |  RGui  |RCtrl |
+ *                                 |LCtrl | LGui | LAlt |       | RAlt |  RGui  |RCtrl |
  *                                 `--------------------'       `----------------------'
  */
 // If it accepts an argument (i.e, is a function), it doesn't need KC_.
@@ -45,18 +45,18 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_BSLS,        KC_A,         KC_S,   KC_D,   KC_F,   KC_G,
         KC_LSFT,        KC_Z,         KC_X,   KC_C,   KC_V,   KC_B,   KC_LBRC,
         KC_HOME,        ALL_T(KC_NO), MEH_T(KC_NO),LGUI(KC_LCTL),KC_LANG2,
-                                                             KC_LALT,KC_LGUI,
+                                                             KC_CAPS,KC_LGUI,
                                                                        RESET,
-                     		           LCTL_T(KC_SPC),LGUI_T(KC_ENT),KC_CAPS,
+                     		           LCTL_T(KC_SPC),LGUI_T(KC_ENT),KC_LALT,
         // right hand
         LALT(KC_SPC), KC_6,   KC_7,  KC_8,   KC_9,   KC_0,             KC_BSPC,
         KC_MINS,      KC_Y,   KC_U,  KC_I,   KC_O,   KC_P,             KC_EQL,
                       KC_H,   KC_J,  KC_K,   KC_L,   KC_SCLN,          KC_QUOT,
         KC_RBRC,      KC_N,   KC_M,  KC_COMM,KC_DOT, KC_SLSH,          KC_RSFT,
                                      KC_LANG1, KC_LEFT,KC_DOWN,KC_UP,KC_RIGHT,
-        KC_RGUI, KC_RALT,
+        KC_RGUI,TG(SYMB),
         TG(MDIA),
-        TG(SYMB),RGUI_T(KC_ENT), RCTL_T(KC_SPC)
+        KC_RALT,RGUI_T(KC_ENT), RCTL_T(KC_SPC)
     ),
 /* Keymap 1: Symbol Layer
  *

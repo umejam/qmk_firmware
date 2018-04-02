5b1ed5f1b65684221de3348854fadb9c05b8ac81
Takeshi Imai <imai-t@casio.co.jp>
2018-04-02 11:28:16 +0900

    Windows用に漢字キー(Alt + `)追加
    その他以下の変更を入れた
    
    * 矢印キーはLayer 1に移動
    * EnterキーはShiftキーと共用に変更

--- keyboards/ergodox_ez/keymaps/umejam/keymap.c
+++ keyboards/ergodox_ez/keymaps/umejam/keymap.c
@@ -18,21 +18,21 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /* Keymap 0: Basic layer
  *
  * ,--------------------------------------------------.           ,--------------------------------------------------.
- * |  ESC   |   1  |   2  |   3  |   4  |   5  | ESC  |           |Alt/Sp|   6  |   7  |   8  |   9  |   0  | BkSp   |
+ * |  漢字  |   1  |   2  |   3  |   4  |   5  |  ESC |            |Alt/Sp|   6  |   7  |   8  |   9  |   0  | BkSp   |
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
  * |  Tab   |   Q  |   W  |   E  |   R  |   T  | `    |           |  -   |   Y  |   U  |   I  |   O  |   P  |   =    |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
  * |   \    |   A  |   S  |   D  |   F  |   G  |------|           |------|   H  |   J  |   K  |   L  |  ;   |   '    |
  * |--------+------+------+------+------+------|  [   |           |   ]  |------+------+------+------+------+--------|
- * | LShift |   Z  |   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |  /   | RShift |
+ * |Ent/Shft|   Z  |   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |  /   |Ent/Shft|
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
- *   |Home  |Hyper | Meh  |CtlGui|LANG2 |                                       |LANG1 | Left | Down |  Up  | Right |
- *   `----------------------------------'                                       `----------------------------------'
+ *   |Space |  L2  |  L1  |CtlGui|LANG2 |                                       |LANG1 |CtlGui|Space | Home | Space |
+ *   `----------------------------------'                                       `-----------------------------------'
  *                                        ,-------------.       ,---------------.
- *                                        | CAPS |RESET |       |  L1  |  L2    |
+ *                                        | CAPS |RESET |       |  Meh | Hyper  |
  *                                 ,------|------|------|       |------+--------+------.
  *                                 |      |      | LGui |       | RGui |        |      |
- *                                 |Space/|Enter/|------|       |------| Enter/ |Space/|
+ *                                 |Space/|      |------|       |------|        |Space/|
  *                                 |LCtrl | LGui | LAlt |       | RAlt |  RGui  |RCtrl |
  *                                 `--------------------'       `----------------------'
  */
@@ -40,39 +40,39 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 // Otherwise, it needs KC_*
 [BASE] = KEYMAP(  // layer 0 : default
         // left hand
-        KC_ESC,         KC_1,         KC_2,   KC_3,   KC_4,   KC_5,   KC_ESC,
-        KC_TAB,         KC_Q,         KC_W,   KC_E,   KC_R,   KC_T,   KC_GRV,
-        KC_BSLS,        KC_A,         KC_S,   KC_D,   KC_F,   KC_G,
-        KC_LSFT,        KC_Z,         KC_X,   KC_C,   KC_V,   KC_B,   KC_LBRC,
-        KC_HOME,        ALL_T(KC_NO), MEH_T(KC_NO),LGUI(KC_LCTL),KC_LANG2,
+		LALT(KC_GRV),   KC_1,       KC_2,    KC_3,         KC_4,    KC_5,   KC_ESC,
+        KC_TAB,         KC_Q,       KC_W,    KC_E,         KC_R,    KC_T,   KC_GRV,
+        KC_BSLS,        KC_A,       KC_S,    KC_D,         KC_F,    KC_G,
+        LSFT_T(KC_ENT), KC_Z,       KC_X,    KC_C,         KC_V,    KC_B,   KC_LBRC,
+        KC_SPC,          TG(MDIA),   TG(SYMB),LGUI(KC_LCTL),KC_LANG2,
 
-		                                                     KC_CAPS,  RESET,
+		                                                    KC_CAPS, RESET,
                                                                      KC_LGUI,
-                     		           LCTL_T(KC_SPC),LGUI_T(KC_ENT),KC_LALT,
+		                                     LCTL_T(KC_SPC),KC_LGUI, KC_LALT,
         // right hand
-        LALT(KC_SPC), KC_6,   KC_7,  KC_8,   KC_9,   KC_0,             KC_BSPC,
-        KC_MINS,      KC_Y,   KC_U,  KC_I,   KC_O,   KC_P,             KC_EQL,
-                      KC_H,   KC_J,  KC_K,   KC_L,   KC_SCLN,          KC_QUOT,
-        KC_RBRC,      KC_N,   KC_M,  KC_COMM,KC_DOT, KC_SLSH,          KC_RSFT,
-                                     KC_LANG1, KC_LEFT,KC_DOWN,KC_UP,KC_RIGHT,
+        LALT(KC_SPC), KC_6,   KC_7,     KC_8,         KC_9,   KC_0,        KC_BSPC,
+        KC_MINS,      KC_Y,   KC_U,     KC_I,         KC_O,   KC_P,        KC_EQL,
+                      KC_H,   KC_J,     KC_K,         KC_L,   KC_SCLN,     KC_QUOT,
+        KC_RBRC,      KC_N,   KC_M,     KC_COMM,      KC_DOT, KC_SLSH,     RSFT_T(KC_ENT),
+		                      KC_LANG1, RGUI(KC_RCTL),KC_SPC, KC_HOME,     KC_SPC,
 
-        TG(SYMB),TG(MDIA),
+        MEH_T(KC_NO),ALL_T(KC_NO),
         KC_RGUI,
-        KC_RALT,RGUI_T(KC_ENT), RCTL_T(KC_SPC)
+        KC_RALT,KC_RGUI,RCTL_T(KC_SPC)
     ),
 /* Keymap 1: Symbol Layer
  *
  * ,---------------------------------------------------.           ,--------------------------------------------------.
  * |Version  |  F1  |  F2  |  F3  |  F4  |  F5  |      |           |      |  F6  |  F7  |  F8  |  F9  |  F10 |   F11  |
  * |---------+------+------+------+------+------+------|           |------+------+------+------+------+------+--------|
- * |         |   !  |   @  |   {  |   }  |   |  |      |           |      |   Up |   7  |   8  |   9  |   *  |   F12  |
+ * |         |   !  |   @  |   {  |   }  |   |  |      |           |      |   7  |   8  |   9  |   *  |  &   |   F12  |
  * |---------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
- * |         |   #  |   $  |   (  |   )  |   `  |------|           |------| Down |   4  |   5  |   6  |   +  |        |
+ * |         |   #  |   $  |   (  |   )  |   `  |------|           |------|   4  |   5  |   6  |   +  |  =   |        |
  * |---------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
- * |         |   %  |   ^  |   [  |   ]  |   ~  |      |           |      |   &  |   1  |   2  |   3  |   \  |        |
+ * |         |   %  |   ^  |   [  |   ]  |   ~  |      |           |      |   1  |   2  |   3  |   \  |  Up  |        |
  * `---------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
- *   | EPRM  |      |      |      |      |                                       |      |    . |   0  |   =  |      |
- *   `-----------------------------------'                                       `----------------------------------'
+ *   | EPRM  |      |      |      |      |                                       |    . |   0  | Left | Down | Right |
+ *   `-----------------------------------'                                       `-----------------------------------'
  *                                        ,-------------.       ,-------------.
  *                                        |Animat|Toggle|       |      |      |
  *                                 ,------|------|------|       |------+------+------.
@@ -94,11 +94,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                                RGB_SLD,
                                RGB_VAD,RGB_VAI,KC_TRNS,
        // right hand
-       KC_TRNS, KC_F6,   KC_F7,  KC_F8,   KC_F9,   KC_F10,  KC_F11,
-       KC_TRNS, KC_UP,   KC_7,   KC_8,    KC_9,    KC_ASTR, KC_F12,
-                KC_DOWN, KC_4,   KC_5,    KC_6,    KC_PLUS, KC_RIGHT,
-       KC_TRNS, KC_AMPR, KC_1,   KC_2,    KC_3,    KC_BSLS, KC_TRNS,
-                         KC_LEFT,KC_DOT,  KC_0,    KC_EQL,  KC_RIGHT,
+       KC_TRNS, KC_F6,   KC_F7,  KC_F8,    KC_F9,   KC_F10,  KC_F11,
+       KC_TRNS, KC_7,    KC_8,    KC_9,    KC_ASTR, KC_AMPR, KC_F12,
+	            KC_4,    KC_5,    KC_6,    KC_PLUS, KC_EQL,  KC_TRNS,
+       KC_TRNS, KC_1,    KC_2,    KC_3,    KC_BSLS, KC_UP,   KC_TRNS,
+	                     KC_DOT,  KC_0,    KC_LEFT, KC_DOWN, KC_RIGHT,
 
        KC_TRNS,KC_TRNS,
        KC_TRNS,

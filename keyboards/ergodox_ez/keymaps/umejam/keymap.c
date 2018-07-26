77c042b5e9e4ef38fb2dacfeac4d9ba189dbb835
Takeshi Imai <imai-t@casio.co.jp>
2018-07-26 09:48:53 +0900

    /と\を入れ替え

--- keyboards/ergodox_ez/keymaps/umejam/keymap.c
+++ keyboards/ergodox_ez/keymaps/umejam/keymap.c
@@ -18,11 +18,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /* Keymap 0: Basic layer
  *
  * ,--------------------------------------------------.           ,--------------------------------------------------.
- * |  ESC   |   1  |   2  |   3  |   4  |   5  |   /  |           |Alt/Sp|   6  |   7  |   8  |   9  |   0  | BkSp   |
+ * |  ESC   |   1  |   2  |   3  |   4  |   5  |   \  |           |Alt/Sp|   6  |   7  |   8  |   9  |   0  | BkSp   |
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
  * |  Tab   |   Q  |   W  |   E  |   R  |   T  | `    |           |  -   |   Y  |   U  |   I  |   O  |   P  |   =    |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
- * |  \     |   A  |   S  |   D  |   F  |   G  |------|           |------|   H  |   J  |   K  |   L  |  ;   |   '    |
+ * |  /     |   A  |   S  |   D  |   F  |   G  |------|           |------|   H  |   J  |   K  |   L  |  ;   |   '    |
  * |--------+------+------+------+------+------|  [   |           |   ]  |------+------+------+------+------+--------|
  * | Shift  |   Z  |   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |  Up  | Shift  |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
@@ -40,9 +40,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 // Otherwise, it needs KC_*
 [BASE] = LAYOUT_ergodox(  // layer 0 : default
         // left hand
-	KC_ESC,         KC_1,       KC_2,    KC_3,         KC_4,    KC_5,   KC_SLSH,
+	KC_ESC,         KC_1,       KC_2,    KC_3,         KC_4,    KC_5,   KC_BSLS,
         KC_TAB,         KC_Q,       KC_W,    KC_E,         KC_R,    KC_T,   KC_GRV,
-        KC_BSLS,        KC_A,       KC_S,    KC_D,         KC_F,    KC_G,
+        KC_SLSH,        KC_A,       KC_S,    KC_D,         KC_F,    KC_G,
         KC_LSFT,        KC_Z,       KC_X,    KC_C,         KC_V,    KC_B,   KC_LBRC,
         LALT(KC_GRV),   TG(MDIA),   TG(SYMB),KC_LANG2,     KC_LALT,
 

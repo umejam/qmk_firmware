1e4021490a1b889ce1e357c90b6c8cb89e62a79e
Takeshi Imai <imai-t@casio.co.jp>
2019-04-19 11:57:43 +0900

    :+1: Ctrl, Commandのダブルファンクションをやめた
    Parallelsで使用した時に、ダブルファンクション設定のキーのオフを取りこ
    ぼすことが頻発したので、ダブルファンクションをやめた

--- keyboards/ergodox_ez/keymaps/umejam/keymap.c
+++ keyboards/ergodox_ez/keymaps/umejam/keymap.c
@@ -26,14 +26,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+------+------+------+------+------|  [   |           |   ]  |------+------+------+------+------+--------|
  * | Shift  |   Z  |   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |  Up  | Shift  |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
- *   |  L2  |  L1  | 漢字 | LANG2 | LAlt |                                       |RAlt  |LANG1 | Left | Down | Right |
+ *   |  L2  |  L1  | 漢字 | LANG2 | Space|                                       |Enter |LANG1 | Left | Down | Right |
  *   `----------------------------------'                                       `-----------------------------------'
  *                                        ,-------------.       ,---------------.
  *                                        | CAPS |RESET |       |  Meh | Hyper  |
  *                                 ,------|------|------|       |------+--------+------.
  *                                 |      |      | Home |       | RGui |        |      |
- *                                 |Space/|Enter/|------|       |------| Enter/ |Space/|
- *                                 |LCtrl | LGui |CtlGui|       |CtlGui|  RGui  |RCtrl |
+ *                                 |      |      |------|       |------|        |      |
+ *                                 |LCtrl | LGui | LAlt |       | RAlt |  RGui  |RCtrl |
  *                                 `--------------------'       `----------------------'
  */
 // If it accepts an argument (i.e, is a function), it doesn't need KC_.
@@ -44,21 +44,21 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,         KC_Q,       KC_W,    KC_E,         KC_R,    KC_T,   KC_GRV,
         KC_SLSH,        KC_A,       KC_S,    KC_D,         KC_F,    KC_G,
         KC_LSFT,        KC_Z,       KC_X,    KC_C,         KC_V,    KC_B,   KC_LBRC,
-        TG(MDIA),       TG(SYMB),   LALT(KC_GRV),KC_LANG2, KC_LALT,
+        TG(MDIA),       TG(SYMB),   LALT(KC_GRV),KC_LANG2, KC_SPC,
 
 		                                                    KC_CAPS, RESET,
                                                                              KC_HOME,
-		                              LCTL_T(KC_SPC),LGUI_T(KC_ENT), LGUI(KC_LCTL),
+		                                           KC_LCTL, KC_LGUI, KC_LALT,
         // right hand
         LALT(KC_SPC), KC_6,   KC_7,     KC_8,         KC_9,   KC_0,        KC_BSPC,
         KC_MINS,      KC_Y,   KC_U,     KC_I,         KC_O,   KC_P,        KC_EQL,
                       KC_H,   KC_J,     KC_K,         KC_L,   KC_SCLN,     KC_QUOT,
         KC_RBRC,      KC_N,   KC_M,     KC_COMM,      KC_DOT, KC_UP,       KC_RSFT,
-	                      KC_RALT,  KC_LANG1,     KC_LEFT,KC_DOWN,     KC_RIGHT,
+	                      KC_ENT,   KC_LANG1,     KC_LEFT,KC_DOWN,     KC_RIGHT,
 
         MEH_T(KC_NO),ALL_T(KC_NO),
         KC_RGUI,
-        RGUI(KC_RCTL),RGUI_T(KC_ENT),RCTL_T(KC_SPC)
+        KC_RALT,KC_RGUI,KC_RCTL
     ),
 /* Keymap 1: Symbol Layer
  *

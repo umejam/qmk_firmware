a3917dade80fc7c462c6d25571886d39f9ca62a0
Takeshi Imai <imai-t@casio.co.jp>
2019-10-03 10:35:08 +0900

    中央4つのキーの配置を変更
    カッコが左右に分かれているのが、やりづらくなってきたので。

--- keyboards/ergodox_ez/keymaps/umejam/keymap.c
+++ keyboards/ergodox_ez/keymaps/umejam/keymap.c
@@ -20,13 +20,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,--------------------------------------------------.           ,--------------------------------------------------.
  * |  ESC   |   1  |   2  |   3  |   4  |   5  |   \  |           |Alt+Sp|   6  |   7  |   8  |   9  |   0  | BkSp   |
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
- * |  Tab   |   Q  |   W  |   E  |   R  |   T  | `    |           |  -   |   Y  |   U  |   I  |   O  |   P  |   =    |
+ * |  Tab   |   Q  |   W  |   E  |   R  |   T  |  [   |           |  `   |   Y  |   U  |   I  |   O  |   P  |   =    |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
  * |  /     |   A  |   S  |   D  |   F  |   G  |------|           |------|   H  |   J  |   K  |   L  |  ;   |   '    |
- * |--------+------+------+------+------+------|  [   |           |   ]  |------+------+------+------+------+--------|
+ * |--------+------+------+------+------+------|  ]   |           |  -   |------+------+------+------+------+--------|
  * | Shift  |   Z  |   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |  Up  | Shift  |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
- *   |  L2  |  L1  |Alt+Sp| LANG2 | SPC  |                                       | SPC  |LANG1 | Left | Down | Right |
+ *   |  L2  |  L1  |Alt+Sp| LANG2| SPC  |                                       | SPC  |LANG1 | Left | Down | Right |
  *   `----------------------------------'                                       `-----------------------------------'
  *                                        ,-------------.       ,---------------.
  *                                        | CAPS |RESET |       |  Meh | Hyper  |
@@ -41,9 +41,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [BASE] = LAYOUT_ergodox(  // layer 0 : default
         // left hand
 	KC_ESC,         KC_1,       KC_2,    KC_3,         KC_4,    KC_5,   KC_BSLS,
-        KC_TAB,         KC_Q,       KC_W,    KC_E,         KC_R,    KC_T,   KC_GRV,
+        KC_TAB,         KC_Q,       KC_W,    KC_E,         KC_R,    KC_T,   KC_LBRC,
         KC_SLSH,        KC_A,       KC_S,    KC_D,         KC_F,    KC_G,
-        KC_LSFT,        KC_Z,       KC_X,    KC_C,         KC_V,    KC_B,   KC_LBRC,
+        KC_LSFT,        KC_Z,       KC_X,    KC_C,         KC_V,    KC_B,   KC_RBRC,
         TG(MDIA),       TG(SYMB),   LALT(KC_SPC),KC_LANG2, KC_SPC,
 
 		                                                    KC_CAPS,RESET,
@@ -51,9 +51,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	                                             LCTL_T(KC_ENT),KC_LGUI,KC_LALT,
         // right hand
         LALT(KC_SPC), KC_6,   KC_7,     KC_8,         KC_9,   KC_0,        KC_BSPC,
-        KC_MINS,      KC_Y,   KC_U,     KC_I,         KC_O,   KC_P,        KC_EQL,
+        KC_GRV,       KC_Y,   KC_U,     KC_I,         KC_O,   KC_P,        KC_EQL,
                       KC_H,   KC_J,     KC_K,         KC_L,   KC_SCLN,     KC_QUOT,
-        KC_RBRC,      KC_N,   KC_M,     KC_COMM,      KC_DOT, KC_UP,       KC_RSFT,
+        KC_MINS,      KC_N,   KC_M,     KC_COMM,      KC_DOT, KC_UP,       KC_RSFT,
 	                      KC_SPC,   KC_LANG1,     KC_LEFT,KC_DOWN,     KC_RIGHT,
 
         MEH_T(KC_NO),ALL_T(KC_NO),

2a82c90bf89ce5730f8d7cc2e7fd9c2d51ca9252
Takeshi Imai <imai-t@casio.co.jp>
2020-06-29 08:55:24 +0900

    Versionの位置を移動
    escキーだと思って押しちゃうことが多いので。

--- keyboards/ergodox_ez/keymaps/umejam/keymap.c
+++ keyboards/ergodox_ez/keymaps/umejam/keymap.c
@@ -63,7 +63,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /* Keymap 1: Symbol Layer
  *
  * ,---------------------------------------------------.           ,--------------------------------------------------.
- * |Version  |  F1  |  F2  |  F3  |  F4  |  F5  |      |           |      |  F6  |  F7  |  F8  |  F9  |  F10 |  BSPC  |
+ * |         |  F1  |  F2  |  F3  |  F4  |  F5  |Ver   |           |      |  F6  |  F7  |  F8  |  F9  |  F10 |  BSPC  |
  * |---------+------+------+------+------+------+------|           |------+------+------+------+------+------+--------|
  * |         |   !  |   @  |   {  |   }  |   |  |      |           |      |   7  |   8  |   9  |   *  |  &   |   F11  |
  * |---------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
@@ -84,7 +84,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 // SYMBOLS
 [SYMB] = LAYOUT_ergodox(
        // left hand
-       VRSN,   KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_F5,  KC_TRNS,
+       KC_TRNS,KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_F5,  VRSN,
        KC_TRNS,KC_EXLM,KC_AT,  KC_LCBR,KC_RCBR,KC_PIPE,KC_TRNS,
        KC_TRNS,KC_HASH,KC_DLR, KC_LPRN,KC_RPRN,KC_GRV,
        KC_TRNS,KC_PERC,KC_CIRC,KC_LBRC,KC_RBRC,KC_TILD,EPRM,

9c0181ca4921ff070dd335a39aa68abec4a71390
Takeshi Imai <imai-t@casio.co.jp>
2020-10-27 09:26:38 +0900

    🐛 Ctrl+[が動作していなかった不具合を修正

--- keyboards/moonlander/keymaps/umejam/keymap.c
+++ keyboards/moonlander/keymaps/umejam/keymap.c
@@ -34,12 +34,12 @@ enum custom_keycodes {
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [BASE] = LAYOUT_moonlander(
-        KC_ESC,         KC_1,    KC_2,   KC_3,   KC_4,  KC_5, KC_F5,         RALT(KC_SPC), KC_6, KC_7,  KC_8,   KC_9,   KC_0,   KC_BSPC,
-        KC_TAB,         KC_Q,    KC_W,   KC_E,   KC_R,  KC_T, CTL_T(KC_LBRC),KC_MINS,      KC_Y, KC_U,  KC_I,   KC_O,   KC_P,   KC_EQL,
-        KC_BSLS,        KC_A,    KC_S,   KC_D,   KC_F,  KC_G, TG(MDIA),      TG(SYMB),     KC_H, KC_J,  KC_K,   KC_L,   KC_SCLN,KC_QUOT,
-        LSFT_T(KC_CAPS),KC_Z,    KC_X,   KC_C,   KC_V,  KC_B,                              KC_N, KC_M,  KC_COMM,KC_DOT, KC_SLSH,RSFT_T(KC_CAPS),
-        KC_LEFT,        KC_RIGHT,A(KC_X),KC_GRV,KC_SPC,       KC_LANG2,      KC_LANG1,           KC_SPC,KC_LBRC,KC_RBRC,KC_UP,  KC_DOWN,
-                                            LCTL_T(KC_ENT),KC_LGUI,KC_LALT,  KC_RALT,KC_RGUI,RCTL_T(KC_ENT)
+        KC_ESC,         KC_1,    KC_2,   KC_3,   KC_4,  KC_5, KC_F5,        RALT(KC_SPC), KC_6, KC_7,  KC_8,   KC_9,   KC_0,   KC_BSPC,
+        KC_TAB,         KC_Q,    KC_W,   KC_E,   KC_R,  KC_T, C(KC_LBRC),   KC_MINS,      KC_Y, KC_U,  KC_I,   KC_O,   KC_P,   KC_EQL,
+        KC_BSLS,        KC_A,    KC_S,   KC_D,   KC_F,  KC_G, TG(MDIA),     TG(SYMB),     KC_H, KC_J,  KC_K,   KC_L,   KC_SCLN,KC_QUOT,
+        LSFT_T(KC_CAPS),KC_Z,    KC_X,   KC_C,   KC_V,  KC_B,                             KC_N, KC_M,  KC_COMM,KC_DOT, KC_SLSH,RSFT_T(KC_CAPS),
+        KC_LEFT,        KC_RIGHT,A(KC_X),KC_GRV,KC_SPC,       KC_LANG2,     KC_LANG1,           KC_SPC,KC_LBRC,KC_RBRC,KC_UP,  KC_DOWN,
+                                            LCTL_T(KC_ENT),KC_LGUI,KC_LALT, KC_RALT,KC_RGUI,RCTL_T(KC_ENT)
     ),
 
     [SYMB] = LAYOUT_moonlander(

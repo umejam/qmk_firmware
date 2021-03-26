cf712be73c573e10a48c932505ce8d9ccb29712b
Takeshi Imai <imai-t@casio.co.jp>
2021-03-26 14:12:26 +0900

    レイヤ変更キーの配置を変更
    ミスタッチでレイヤが切り替わることが多いので、配置変更

--- keyboards/moonlander/keymaps/umejam/keymap.c
+++ keyboards/moonlander/keymaps/umejam/keymap.c
@@ -34,20 +34,20 @@ enum custom_keycodes {
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [BASE] = LAYOUT_moonlander(
-        KC_ESC,         KC_1,    KC_2,   KC_3,   KC_4,  KC_5, KC_F5,        RALT(KC_SPC), KC_6, KC_7,  KC_8,   KC_9,   KC_0,   KC_BSPC,
-        KC_TAB,         KC_Q,    KC_W,   KC_E,   KC_R,  KC_T, C(KC_LBRC),   KC_MINS,      KC_Y, KC_U,  KC_I,   KC_O,   KC_P,   KC_EQL,
-        KC_BSLS,        KC_A,    KC_S,   KC_D,   KC_F,  KC_G, TG(MDIA),     TG(SYMB),     KC_H, KC_J,  KC_K,   KC_L,   KC_SCLN,KC_QUOT,
-        LSFT_T(KC_CAPS),KC_Z,    KC_X,   KC_C,   KC_V,  KC_B,                             KC_N, KC_M,  KC_COMM,KC_DOT, KC_SLSH,RSFT_T(KC_CAPS),
-        KC_LEFT,        KC_RIGHT,A(KC_X),KC_GRV,KC_SPC,       KC_LANG2,     KC_LANG1,           KC_SPC,KC_LBRC,KC_RBRC,KC_UP,  KC_DOWN,
-                                            LCTL_T(KC_ENT),KC_LGUI,KC_LALT, KC_RALT,KC_RGUI,RCTL_T(KC_ENT)
+        KC_ESC,  KC_1,    KC_2,   KC_3,   KC_4,  KC_5, KC_F5,        RALT(KC_SPC),KC_6,  KC_7,  KC_8,    KC_9,    KC_0,   KC_BSPC,
+        KC_TAB,  KC_Q,    KC_W,   KC_E,   KC_R,  KC_T, KC_LBRC,      KC_RBRC,     KC_Y,  KC_U,  KC_I,    KC_O,    KC_P,   KC_EQL,
+        KC_BSLS, KC_A,    KC_S,   KC_D,   KC_F,  KC_G, C(KC_LBRC),   KC_MINS,     KC_H,  KC_J,  KC_K,    KC_L,    KC_SCLN,KC_QUOT,
+        KC_LSFT, KC_Z,    KC_X,   KC_C,   KC_V,  KC_B,                            KC_N,  KC_M,  KC_COMM, KC_DOT,  KC_SLSH,KC_RSFT,
+        KC_LEFT, KC_RIGHT,A(KC_X),KC_GRV, KC_SPC,      KC_LANG2,     KC_LANG1,           KC_SPC,TG(SYMB),TG(MDIA),KC_UP,  KC_DOWN,
+                                 LCTL_T(KC_ENT),KC_LGUI,KC_LALT,     KC_RALT,KC_RGUI,RCTL_T(KC_ENT)
     ),
 
     [SYMB] = LAYOUT_moonlander(
         VRSN,    KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   WEBUSB_PAIR,       KC_TRNS, KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,
-        KC_TRNS, KC_EXLM, KC_AT,   KC_LCBR, KC_RCBR, KC_PIPE, RESET,             KC_TRNS, KC_UP,   KC_7,    KC_8,    KC_9,    KC_ASTR, KC_F12,
-        KC_TRNS, KC_HASH, KC_DLR,  KC_LPRN, KC_RPRN, KC_GRV,  KC_TRNS,           KC_TRNS, KC_DOWN, KC_4,    KC_5,    KC_6,    KC_PLUS, KC_TRNS,
-        KC_TRNS, KC_PERC, KC_CIRC, KC_LBRC, KC_RBRC, KC_TILD,                             KC_AMPR, KC_1,    KC_2,    KC_3,    KC_BSLS, KC_TRNS,
-        EEP_RST, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,          RGB_VAI,           RGB_TOG,          KC_TRNS, KC_DOT,  KC_0,    KC_EQL,  KC_TRNS,
+        KC_TRNS, KC_EXLM, KC_AT,   KC_LCBR, KC_RCBR, KC_PIPE, RESET,             KC_TRNS, KC_EQL,  KC_7,    KC_8,    KC_9,    KC_PLUS, KC_F12,
+        KC_TRNS, KC_HASH, KC_DLR,  KC_LPRN, KC_RPRN, KC_GRV,  KC_TRNS,           KC_TRNS, KC_DOT,  KC_4,    KC_5,    KC_6,    KC_MINS, KC_TRNS,
+        KC_TRNS, KC_PERC, KC_CIRC, KC_LBRC, KC_RBRC, KC_TILD,                             KC_0,    KC_1,    KC_2,    KC_3,    KC_ASTR, KC_TRNS,
+        EEP_RST, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,          RGB_VAI,           RGB_TOG,          KC_TRNS, KC_TRNS, KC_TRNS, KC_BSLS, KC_AMPR,
                                             RGB_HUD, RGB_VAD, RGB_HUI, TOGGLE_LAYER_COLOR,KC_TRNS, KC_TRNS
     ),
 
@@ -56,7 +56,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TRNS, KC_TRNS, KC_TRNS, KC_MS_U, KC_TRNS, KC_TRNS, KC_TRNS,           KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
         KC_TRNS, KC_TRNS, KC_MS_L, KC_MS_D, KC_MS_R, KC_TRNS, KC_TRNS,           KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_MPLY,
         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                             KC_TRNS, KC_TRNS, KC_MPRV, KC_MNXT, KC_TRNS, KC_TRNS,
-        KC_TRNS, KC_TRNS, KC_TRNS, KC_BTN1, KC_BTN2,         KC_TRNS,            KC_TRNS,               KC_VOLU, KC_VOLD, KC_MUTE, KC_TRNS, KC_TRNS,
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_BTN1, KC_BTN2,          KC_VOLU,           KC_VOLD,          KC_MUTE, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
                                             KC_TRNS, KC_TRNS, KC_TRNS,           KC_TRNS, KC_TRNS, KC_TRNS
     ),
 };
